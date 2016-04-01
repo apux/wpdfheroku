a41793a8f29f426f8f193c66e66e7a03a6777541
Azarel Doroteo Pacheco <azarel.doroteo@logicalbricks.com>
2016-04-01 14:09:31 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wpdfheroku::Application.config.secret_token = '34a233676d2dfe7020468c2b741d31ce57aabf9dbca891795840a40dc48a78a855b269623d2b071765e2d2f9e3b6acb618a26be0a3230127c08e5304d2982e15'

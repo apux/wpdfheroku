086bbd83c0f22af4b4b475cb4673aa86568f6755
Azarel Doroteo Pacheco <azarel.doroteo@logicalbricks.com>
2016-04-01 16:22:23 -0600

    Add response for pdf

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,23 @@
 class HomeController < ApplicationController
   def index
+    respond_to do |format|
+      format.html
+      format.pdf { render pdf: "file_name" }
+    end
+  end
+
+private
+
+  def pdf
+    options = {
+      pdf: 'myfile',
+      page_size: 'Letter',
+      template: 'home/pdfs/template/body',
+      layout: 'pdf_layout.html',
+      margin: { top: '85', bottom: '80' },
+      header: { html: { template: 'home/pdfs/template/header' } },
+      footer: { html: { template: 'home/pdfs/template/footer'} }
+    } 
+    render_to_string_with_wicked_pdf(options)
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Wpdfheroku
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    require 'wicked_pdf'
+    config.middleware.use WickedPdf::Middleware
   end
 end
+
